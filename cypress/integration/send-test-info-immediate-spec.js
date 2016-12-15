50d3d8432737e76ec132f54a9fd63f5d1a1fc9bc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-12-15 10:41:02 -0500

    chore(test): testing immediate vs beforeEach registration

--- cypress/integration/send-test-info-immediate-spec.js
+++ cypress/integration/send-test-info-immediate-spec.js
@@ -4,14 +4,17 @@ const options = {
   maxCheckTimes: 20,
   maxRavenInstalls: 2,
   interval: 1000,
-  debug: true
+  debug: true,
+  immediate: true
 }
-sendTestInfo(options)
 
-describe('Test page', function(){
+describe('Test page (immediate registration)', function(){
   it('cy.should - assert that <title> is correct', function(){
     cy.visit(__dirname + '/../index.html')
     cy.title().should('eq', 'test')
+
+    sendTestInfo(options)
+
     console.log('sleeping for 15 seconds')
     cy.wait(15000)
   })
